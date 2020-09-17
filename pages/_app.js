0dd7dfa177445d72bfa81305223518c4217f924f
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-17 19:30:54 +0800

    added error checking

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import '../styles/globals.css'
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
 import '../styles/nprogress.css'; //styles of nprogress
+//styletron imports
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider} from 'baseui';
 
 //nprogress config
 NProgress.configure({ showSpinner: false })
@@ -12,7 +16,17 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //styletron
+  if(typeof window === `undefined` || typeof window === null) return <p>loading</p> 
+  const engine = new Styletron();
+
+  return (
+     <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  )
 }
 
 export default MyApp
