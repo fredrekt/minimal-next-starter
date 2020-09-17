537251a14d9ba0cdc739ddaca98d86bacdcd17e8
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-18 07:43:12 +0800

    updated seo

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,34 @@ import Head from 'next/head';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-const Layout = ({page, children}) => {
+const Layout = ({page, children, desc, img}) => {
     const appname = process.env.NEXT_PUBLIC_APP_NAME
+    const title = page ? `${page} | ${appname}`: appname
+    const description = desc ? desc : "sample description here"
+    const image = img ? img : ""
+    const website = process.env.NEXT_PUBLIC_WEBSITE
+
     return (
         <>
         <Head>
-            <title>{page ? `${page} | ${appname}`: appname}</title>
+            <title>{title}</title>
+            {/* <!-- Primary Meta Tags --> */}
+            <meta name="title" content={title}/>
+            <meta name="description" content={description}/>
+
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content={website}/>
+            <meta property="og:title" content={title}/>
+            <meta property="og:description" content={description}/>
+            <meta property="og:image" content={image}/>
+
+            {/* <!-- Twitter --> */}
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content={website}/>
+            <meta property="twitter:title" content={title}/>
+            <meta property="twitter:description" content={description}/>
+            <meta property="twitter:image" content={image}/>
         </Head>
         <Navbar/>
           <main>{children}</main>
