534a48bc2e9ceeb4b39cbff07c6f52b2e6423573
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-17 18:12:57 +0800

    added nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
 import '../styles/globals.css'
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import '../styles/nprogress.css'; //styles of nprogress
+
+//nprogress config
+NProgress.configure({ showSpinner: false })
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
