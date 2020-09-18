d77ad1d3d27d07d7f81dafcb558e3331ee005036
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-18 08:09:45 +0800

    added favicon and code cleanup

--- components/Layout.js
+++ components/Layout.js
@@ -14,18 +14,21 @@ const Layout = ({page, children, desc, img}) => {
         <>
         <Head>
             <title>{title}</title>
-            {/* <!-- Primary Meta Tags --> */}
             <meta name="title" content={title}/>
             <meta name="description" content={description}/>
 
-            {/* <!-- Open Graph / Facebook --> */}
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+
             <meta property="og:type" content="website"/>
             <meta property="og:url" content={website}/>
             <meta property="og:title" content={title}/>
             <meta property="og:description" content={description}/>
             <meta property="og:image" content={image}/>
 
-            {/* <!-- Twitter --> */}
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content={website}/>
             <meta property="twitter:title" content={title}/>
