8908fe4e48a76f7f0d63212586bbde0a68b6994b
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-20 15:10:07 +0800

    added consent for the gdpr policy use of cookies

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import {
+    Modal,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    ModalButton,
+    SIZE,
+    ROLE
+  } from "baseui/modal";
+import { StyledLink } from 'baseui/link';
 
 const Layout = ({page, children, desc, img}) => {
+    const [show, setShow] = useState(typeof window !== `undefined` && localStorage.getItem('consent') === null && true);
     const appname = process.env.NEXT_PUBLIC_APP_NAME
     const title = page ? `${page} | ${appname}`: appname
     const description = desc ? desc : "sample description here"
     const image = img ? img : ""
     const website = process.env.NEXT_PUBLIC_WEBSITE
+
+    useEffect(() => {
+        localStorage.setItem('consent', show)
+    }, [show])
 
     return (
         <>
@@ -38,6 +53,25 @@ const Layout = ({page, children, desc, img}) => {
         <Navbar/>
           <main>{children}</main>
         <Footer/>
+        <Modal
+            onClose={() => setShow(false)}
+            closeable
+            isOpen={show}
+            animate
+            autoFocus
+            size={SIZE.default}
+            role={ROLE.dialog}
+        >
+        <ModalHeader>Cookies Policy</ModalHeader>
+        <ModalBody>
+        Your privacy is important to us. It is Fredrekt's policy to respect 
+        your privacy regarding any information we may collect from you across our website, 
+        http://next-minimalist.ga/, and other sites we own and operate. <StyledLink href="/privacypolicy">Read more about the policy.</StyledLink>
+        </ModalBody>
+        <ModalFooter>
+            <ModalButton onClick={()=>setShow(false)}>Okay</ModalButton>
+        </ModalFooter>
+        </Modal>
         </>
     )
 }
